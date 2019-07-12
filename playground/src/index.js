01c6dd17cee0ceedb87536765d011f4d35dc1713
smrsan76 <smrsan76@users.noreply.github.com>
2019-07-13 01:44:09 +0430

    Test `Displaying multiple months` in playground

--- playground/src/index.js
+++ playground/src/index.js
@@ -10,18 +10,39 @@ const App = () => {
     { day: 11, month: 4, year: 1398 },
     { day: 15, month: 4, year: 1398 },
   ]);
+  const [numOfMonths, setNumOfMonths] = useState(1);
+
+  const handleMonthNumChange = e => {
+    const { value } = e.target;
+    setNumOfMonths(Number(value));
+  };
+
   const displayMonthsFrom = { year: 1398, month: 1 };
-  const displayMonthsTo = { year: 1398, month: 12 };
 
   return (
-    <Calendar
-      selectedDays={selectedDays}
-      onChange={setValue}
-      isMultipleDays
-      showMultipleMonths
-      displayMonthsFrom={displayMonthsFrom}
-      displayMonthsTo={displayMonthsTo}
-    />
+    <>
+      <label htmlFor="num">
+        Number of months:
+        <input
+          id="num"
+          type="number"
+          value={numOfMonths}
+          min={1}
+          max={12}
+          onChange={handleMonthNumChange}
+        />
+      </label>
+      <br />
+      <br />
+      <Calendar
+        isMultipleDays
+        showMultipleMonths
+        displayMonthsFrom={displayMonthsFrom}
+        displayMonthsTo={{ year: 1398, month: numOfMonths }}
+        selectedDays={selectedDays}
+        onChange={setValue}
+      />
+    </>
   );
 };
 
